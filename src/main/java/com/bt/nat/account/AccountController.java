ae693c33f67f546debee0c8e185cd78daef60097
rajeevchotai <rajeevchotai@gmail.com>
2014-02-12 19:38:21 +0530

    more changes
    modified lots of files

--- src/main/java/com/bt/nat/account/AccountController.java
+++ src/main/java/com/bt/nat/account/AccountController.java
@@ -2,15 +2,23 @@ package com.bt.nat.account;
 
 import java.security.Principal;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bt.nat.signup.SignupForm;
+import com.bt.nat.support.web.MessageHelper;
 
 @Controller
 @Secured("ROLE_USER")
@@ -30,4 +38,9 @@ class AccountController {
         Assert.notNull(principal);
         return accountRepository.findByEmail(principal.getName());
     }
+    
+	@RequestMapping(value = "user/add")
+	public String signup() {
+		return "user/add";
+	}
 }
